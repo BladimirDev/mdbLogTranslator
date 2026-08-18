43f85b88fc1198d62218a359f4484f52fa92ee54
bladimir_villarroel <bladimir.dev.contato@gmail.com>
2026-08-18 14:41:07 -0300

    Testando as saídas do arquivo

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 with open("mdb/cashless.mdb") as f:
     lines = f.readlines()
 
-print(lines[1])
-print(lines[0])
 print(lines[2])
-print(lines[3])
+
+#linha = lines[2]
+
+#tms = linha[0:19]
+#tamanho = linha[32:35]
+#opencode = linha[39:53]
+#dados_mdb = linha[42:62]
+#control_bytes = linha[54:62]
+
+#print("Time: ", tms)
+#print("Tamanho: ", tamanho)
+#print("Opencode: ", opencode)
+#print("Dados MDB: ", dados_mdb)
+#print("Control Bytes:", control_bytes)
 #for line in lines:
     #print(line)
