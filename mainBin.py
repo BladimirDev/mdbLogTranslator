68eb4e8211e534ea782c7be3b3f2cc4dc7b467c5
bladimir_villarroel <bladimir.dev.contato@gmail.com>
2026-08-19 10:10:37 -0300

    Acertei o nome de algumas variáveis

--- mainBin.py
+++ mainBin.py
@@ -2,39 +2,31 @@ with open("mdb/cashless.mdb") as arquivo:
     for linha in arquivo: 
         linha = linha.strip()
 
-        partes = linha.split(" ", 3)
+        parts = linha.split(" ", 3)
 
-        data = partes[0]
-        hora = partes[1]
-        sep = partes[2]
-        conteudo = partes[3]
+        data = parts[0]
+        hora = parts[1]
+        sep = parts[2]
+        content = parts[3]
 
         print("Timestamp", data, hora)
         
         #se for um registro hexadecimal
-        if conteudo.startswith("MDB msg: "):
+        if content.startswith("MDB msg: "):
 
-            inicio = conteudo.find("(")
-            fim = conteudo.rfind(")")
+            inicio = content.find("(")
+            fim = content.rfind(")")
 
-            dados = conteudo[inicio + 1:fim].strip()
+            dados = content[inicio + 1:fim].strip()
             print("!" ,dados , "!")
+            dados_separated = dados.split()
 
-            dados_separados = dados.split() 
-
-            for bit in dados_separados[2:]:
+            for bit in dados_separated[2:]:
                 bin_valor= bin(int(bit, 16))
                 clean_bin = bin_valor[2:]
                 print(clean_bin)
-            
-            # print("Data:", data)
-            # print("Hora: ", hora)
-            # print("Dados: ",restante)
             print("----------hex------------")
 
         else:
-            # É uma mensagem natural
-            # print("Data:", data)
-            # print("Hora: ", hora)
-            print("Nao e uma mensagem MDB, mas o conteudo e:", conteudo)
+            print("Nao e uma mensagem MDB, mas o conteudo e:", content)
             print("----------nat------------")
