a0da3b9649c665b75b91d8afe401f6dda37c93d3
bladimir_villarroel <bladimir.dev.contato@gmail.com>
2026-08-20 15:09:31 -0300

    Avancei a formatação das saídas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import binascii
 
+from debian.changelog import endline
+
 #definição das variáveis globais
 ultimo_comando = None
 ultimo_dados = None
@@ -23,6 +25,7 @@ def exibir_dados(linha, data, hora, dados_bin, tam, control_bytes):
     print("Control Bytes:", control_bytes.hex(" "))
 
 
+
 #   definição da função de parseVM
 def parseVM(dados_bin, tam):
 
@@ -44,16 +47,18 @@ def parseVM(dados_bin, tam):
     exibir_dados(linha.strip(), data, hora, dados_bin, tam, control_bytes)
 
     match ultimo_comando:
+        #caso Reset
         case 0x10 | 0x60:
             print("Comando: Reset")
 
+        #caso Setup
         case 0x11 | 0x61:
             print("Comando: Setup")
-            subcomando = dados_bin[2]
+            subcommand = dados_bin[2]
             config_data = dados_bin[2:]
             print("Config data: ", config_data.hex(" "))
 
-            match subcomando:
+            match subcommand:
                 case 0x00:
                     for i in range(0, len(config_data), 2):
                         match i:
@@ -61,13 +66,13 @@ def parseVM(dados_bin, tam):
                                 option = config_data[i]
                                 match option:
                                     case 0x01:
-                                        print("Config 1: Config relativa a 0x01")
+                                        print("The VMC is not capable or will not perform the advanced features as specified in Table 1: COMMANDS & RESPONSES following; Section 7.3.2. The reader will not provide advanced information to; the VMC, but can do the advanced features internally (transparently; to the VMC). The reader has no revaluation capability.")
 
                                     case 0x02:
-                                        print("Config 1: Config relativa a 0x02")
+                                        print("Config 1: The VMC is capable and willing to perform the advanced features as; specified in Table 1: COMMANDS & RESPONSES following; Section 7.3.2. The reader will provide advanced information to the; VMC (if possible) and will not do the advanced features internally.")
 
                                     case 0x03:
-                                        print("Config 1: Config relativa a 0x03")
+                                        print("Config 1: The VMC is able to support level 02, but also supports some or all of; the optional features listed in the EXPANSION ID command (i.e., file; transfer, 32 bit credit, multi-currency / language features, negative; vend, and / or data entry).")
 
                             case 4:
                                 print("Config 2: Columns on Display. The number of columns on the display. Set to 00H if the display is not available to the reader.")
@@ -98,16 +103,142 @@ def parseVM(dados_bin, tam):
                             case 6 | 8:
                                 print("Y4-Y5")
 
+        #caso Poll
         case 0x12 | 0x62:
             print("Comando: Poll")
+
+        #caso Vend
         case 0x13 | 0x63:
+            config_data = dados_bin[2:]
+            subcommand = dados_bin[2]
+
             print("Comando: Vend")
+            print("Subcomando: ", end="")
+
+            match subcommand:
+                #Vend Request
+                case 0x00:
+                    print("Vend Request")
+
+                #Vend Cancel
+                case 0x01:
+                    print("Vend Cancel")
+
+                #Vend Sucess
+                case 0x02:
+                    print('Vend Sucess')
+
+                #Vend Failure
+                case 0x03:
+                    print('Vend Failure')
+
+                #Session Complete
+                case 0x04:
+                    print("Session complete")
+
+                #Cash Sale
+                case 0x05:
+                    print("Cash Sale")
+
+                #Negative Vend Request
+                case 0x06:
+                    print("Negative Vend Request")
+
+        #caso Reader
         case 0x14 | 0x64:
+            config_data = dados_bin[2:]
+            subcommand = dados_bin[2]
+
             print("Comando: Reader")
+            print("Subcomando: ", end="")
+
+            match subcommand:
+                #Reader Disable
+                case 0x00:
+                    print('Reader Disable')
+
+                #Reader Enable
+                case 0x01:
+                    print('Reader Enable')
+
+                #Reader Cancel
+                case 0x02:
+                    print('Reader Cancel')
+
+                #Data Entry Response
+                case 0x03:
+                    print('Data Entry; Response')
+
+        #caso Revalue
         case 0x15 | 0x65:
+            config_data = dados_bin[2:]
+            subcommand = dados_bin[2]
+
             print("Comando: Revalue")
+            print("Subcomando: ", end="")
+
+            match subcommand:
+                #Revalue Request
+                case 0x00:
+                    print('Revalue Request')
+
+                #Revalue Limit Request
+                case 0x01:
+                    print('Revalue Limit Request')
+
+        #caso Expansion
         case 0x17 | 0x67:
+            config_data = dados_bin[2:]
+            subcommand = dados_bin[2]
+
             print("Comando: Expansion")
+            print("Subcomando: ", end="")
+
+            match subcommand:
+                #Request ID
+                case 0x00:
+                    print('Request ID')
+
+                #Read User File
+                case 0x01:
+                    print('Read User File')
+
+                #Write User File
+                case 0x02:
+                    print('Write User File')
+
+                #White time/Date
+                case 0x03:
+                    print('White time/Date')
+
+                #Optional No Data Feature Enabled
+                case 0x04:
+                    print('Optional No Data Feature Enabled')
+
+                #FTL REQ TO RCV
+                case 0xFA:
+                    print('FTL REQ TO RCV')
+
+                #FTL RETRY / DENY
+                case 0xFB:
+                    print('FTL RETRY / DENY')
+
+                #FTL SEND BLOCK
+                case 0xFC:
+                    print('FTL SEND BLOCK')
+
+                #FTL OK TO SEND
+                case 0xFD:
+                    print('FTL OK TO SEND')
+
+                #FTL REQ TO SEND
+                case 0xFE:
+                    print('FTL REQ TO SEND')
+
+                #Diagnostics
+                case 0xFF:
+                    print('Diagnostics')
+
 
 #   definição da função de parseDevice
 def parserDevice(dados_bin, tam):
@@ -124,9 +255,11 @@ def parserDevice(dados_bin, tam):
     print("Resposta para o ultimo comando: ", end="")
 
     match ultimo_comando:
+        #caso Reset
         case 0x10 | 0x60:
             print("#---10")
 
+        #caso Setup
         case 0x11 | 0x61:
             subcomando = dados_bin[2]
             config_data = dados_bin[2:]
@@ -135,7 +268,7 @@ def parserDevice(dados_bin, tam):
                     print("Config data: ")
                     for i in range(0, len(config_data), 2):
                         match i:
-                            case 2:
+                            case 2: #Z2
                                 match config_data[i]:
                                     case 0x01:
                                         print("Config 1: Config relativa a 0x01")
@@ -146,6 +279,21 @@ def parserDevice(dados_bin, tam):
                                     case 0x03:
                                         print("Config 1: Config relativa a 0x03")
 
+                            case 4 | 6: #Z3-Z4
+                                print("Configuração refente a Z3 e Z4")
+                            case 8: #Z5
+                                print("Configuração referente a Z5")
+                            case 10: #Z6
+                                print("Configuração refente a Z6")
+                            case 12: #Z7
+                                print("Configuração referente a Z7")
+                            case 14: #Z8
+                                print("Configuração refente a Z8")
+
+                case 0x01:
+                    print("No data *")
+
+        #caso Poll
         case 0x12 | 0x62:
             resp = dados_bin[0]
             match resp:
@@ -170,15 +318,19 @@ def parserDevice(dados_bin, tam):
                 case 0x09:
                     print("Peripheral ID")
 
+        #caso Vend
         case 0x13 | 0x63:
             print("#---13")
 
+        #caso Reader
         case 0x14 | 0x64:
             print("#---14")
 
+        #caso Revalue
         case 0x15 | 0x65:
             print("#---15")
 
+        #caso Expansion
         case 0x17 | 0x67:
             print("#---17")
 
@@ -220,7 +372,7 @@ with open("mdb/cashless.mdb") as arquivo:
             dados_bin = binascii.unhexlify(dados)
 
             #Somente continua se for igual a F1 ou F2
-            if not tipo == "F1" or tipo == "F2":
+            if tipo not in ("F1", "F2"):
                 continue
 
             #Definição do tamanho da mensagem
